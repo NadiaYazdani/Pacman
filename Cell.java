2bb37259fefca0393c10ecb0daf8331eda295d3c
NadiaYazdani <32622374+NadiaYazdani@users.noreply.github.com>
2019-05-11 13:13:48 +0430

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,25 @@
+class Cell {
+    private int row , col ;
+
+    Cell (int r, int c) {
+        this.row = r ;
+        this.col = c ;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+}
